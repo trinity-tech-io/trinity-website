e6edea7eec453cf1e8b7c53a1443115cf514ae18
blockchaindev91 <hisamitsuteru@gmail.com>
2022-08-01 23:49:19 +0900

    feat: integrate with firebase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React from "react";
 import { useTranslation, Trans } from "react-i18next";
+import firebase from "firebase/compat/app";
+import { getAnalytics } from "firebase/analytics";
 
-import ConicGradiSection from '../components/ConicGradiSection';
-import Schema from '../components/Home/Schema';
-import Summary from '../components/Home/Summary';
+import ConicGradiSection from "../components/ConicGradiSection";
+import Schema from "../components/Home/Schema";
+import Summary from "../components/Home/Summary";
+import { firebaseConfig } from "../config";
+
+const app = firebase.initializeApp(firebaseConfig);
+getAnalytics(app);
 
 const Home = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <>
-            <ConicGradiSection title={<Trans>We build (on) Elastos.</Trans>} titleAlign="center" scrollDown={true}/>
-            <Schema />
-            <Summary type='sdk' />
-            <Summary type='app' />
-        </>
-    );
-}
+  return (
+    <>
+      <ConicGradiSection
+        title={<Trans>We build (on) Elastos.</Trans>}
+        titleAlign="center"
+        scrollDown={true}
+      />
+      <Schema />
+      <Summary type="sdk" />
+      <Summary type="app" />
+    </>
+  );
+};
 
 export default Home;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 // layouts
-import Home from '../pages/Home';
-import Team from '../pages/about/Team';
-import Contact from '../pages/about/Contact';
-import DID from '../pages/sdks/DID';
-import Hive from '../pages/sdks/Hive';
-import Carrier from '../pages/sdks/Carrier';
-import Wallet from '../pages/sdks/Wallet';
-import Essentials from '../pages/apps/Essentials';
-import Feeds from '../pages/apps/Feeds';
-import Pasar from '../pages/apps/Pasar';
-import Elab from '../pages/apps/Elab';
-import KYCme from '../pages/apps/KYCme';
-import Layout from '../layout';
+import Home from "../pages/Home";
+import Team from "../pages/about/Team";
+import Contact from "../pages/about/Contact";
+import DID from "../pages/sdks/DID";
+import Hive from "../pages/sdks/Hive";
+import Carrier from "../pages/sdks/Carrier";
+import Wallet from "../pages/sdks/Wallet";
+import Essentials from "../pages/apps/Essentials";
+import Feeds from "../pages/apps/Feeds";
+import Pasar from "../pages/apps/Pasar";
+import Elab from "../pages/apps/Elab";
+import KYCme from "../pages/apps/KYCme";
+import Layout from "../layout";
 
 // ----------------------------------------------------------------------
 export default function Router() {
   const { pathname } = useLocation();
-  React.useEffect(()=>{
-    window.initMethod()
-    window.scrollTo({top: 0, behavior: 'smooth'})
-  }, [pathname])
+  React.useEffect(() => {
+    window.initMethod();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
   return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="team" element={<Team />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="did" element={<DID />} />
-          <Route path="hive" element={<Hive />} />
-          <Route path="carrier" element={<Carrier />} />
-          <Route path="wallet" element={<Wallet />} />
-          <Route path="essentials" element={<Essentials />} />
-          <Route path="feeds" element={<Feeds />} />
-          <Route path="pasar" element={<Pasar />} />
-          <Route path="e-lab" element={<Elab />} />
-          <Route path="kyc-me" element={<KYCme />} />
-        </Route>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="team" element={<Team />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="did" element={<DID />} />
+        <Route path="hive" element={<Hive />} />
+        <Route path="carrier" element={<Carrier />} />
+        <Route path="wallet" element={<Wallet />} />
+        <Route path="essentials" element={<Essentials />} />
+        <Route path="feeds" element={<Feeds />} />
+        <Route path="pasar" element={<Pasar />} />
+        <Route path="e-lab" element={<Elab />} />
+        <Route path="kyc-me" element={<KYCme />} />
+      </Route>
+    </Routes>
+  );
 }
