9097553f6d3fff4cc22480783ea2f2b5721dd968
blockchaindev91 <hisamitsuteru@gmail.com>
2022-08-02 19:53:01 +0900

    fix: update firebase integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Trans } from "react-i18next";
 import firebase from "firebase/compat/app";
-import { getAnalytics } from "firebase/analytics";
 
 import ConicGradiSection from "../components/ConicGradiSection";
 import Schema from "../components/Home/Schema";
@@ -10,7 +9,7 @@ import { firebaseConfig } from "../config";
 
 if (firebaseConfig.apiKey) {
   const app = firebase.initializeApp(firebaseConfig);
-  getAnalytics(app);
+  firebase.analytics(app);
 }
 
 const Home = () => {
